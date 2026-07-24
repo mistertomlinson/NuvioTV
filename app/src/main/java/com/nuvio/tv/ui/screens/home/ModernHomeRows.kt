b85b0a2b98c6547608f563892d8e174bde93173b
mistertomlinson <brandon.tomlinson@gmail.com>
2026-07-24 13:53:22 -0400

    test(home): shorten matched row scroll springs

--- app/src/main/java/com/nuvio/tv/ui/screens/home/ModernHomeRows.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/home/ModernHomeRows.kt
@@ -566,7 +566,7 @@ internal fun ModernRowSection(
                 override val scrollAnimationSpec: AnimationSpec<Float> =
                     androidx.compose.animation.core.spring(
                         dampingRatio = 0.95f,
-                        stiffness = 400f
+                        stiffness = 600f
                     )
 
                 override fun calculateScrollDistance(
